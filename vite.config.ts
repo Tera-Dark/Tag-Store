0353a0d4bbd833b27cbd3d0b759e5ae4a545b9e9
Tera-Dark <wjx19825323597@gmail.com>
2025-04-22 23:17:23 +0800

    修复GitHub Pages部署问题：更新资源路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Tag-Store-v1.0/',
+  base: '/Tag-Store/',
   plugins: [
     vue(),
   ],
